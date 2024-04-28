8aff0ea8a04e5a590fe6b250f27f464016056646
Anshuman Narayan <anshu.nryn@gmail.com>
2024-04-28 13:01:50 -0400

    updating init to require new files

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("config.image")
+require("config.molten")
+vim.g.python3_host_prog = vim.fn.expand("~/.virtualenvs/neovim/bin/python3")
